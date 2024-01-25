b38e82401d99d3075acb7b8b5f38f83dae570599
salamonszilard <salamonszilard@gmail.com>
2024-01-25 01:03:14 +0100

    vite config update

--- margin_react/vite.config.ts
+++ margin_react/vite.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+
+    rollupOptions: {
+      onLog(level, log, handler) {
+        if (
+          log.cause &&
+          log.cause.message === `Can't resolve original location of error.`
+        ) {
+          return;
+        }
+        handler(level, log);
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }]
-  }
-})
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+});
